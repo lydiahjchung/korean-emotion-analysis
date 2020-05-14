21df4013272a96dca79a13aa8754af5e895c87d0
Tree-Nulbo <exopas58@gmail.com>
2020-05-15 00:03:15 +0900

    Update papago.py

--- NMT_API/papago.py
+++ NMT_API/papago.py
@@ -9,32 +9,31 @@ import json
 from pprint import pprint
 
 # Read sentence you want to translate
-with open('Crawler\\preprocessed_files\\pre_data_0401_0402.txt', 'r', encoding='utf-8') as f:
+with open('..\\..\\Crawler\\preprocessed_files\\twittter_pre_data_0401_0402.txt', 'r', encoding='utf-8') as f:
     raw_sentence = f.read()
 
 # API Key
 client_id = "NJGTWFNflleFvDR2wvqu" 
-client_secret = "Type Password" 
+client_secret = "temp" 
 
-# Naver Papago Open API
-encText = urllib.parse.quote(raw_sentence)
-data = "source=ko&target=en&text=" + encText
-url = "https://openapi.naver.com/v1/papago/n2mt"
-request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id",client_id)
-request.add_header("X-Naver-Client-Secret",client_secret)
-response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-rescode = response.getcode()
+with open('./translated_0401_0402.txt', 'w', encoding='utf8') as f:
+  for sentence in sentence_list:
+    encText = urllib.parse.quote(sentence)
+    data = "source=ko&target=en&text=" + encText
+    url = "https://openapi.naver.com/v1/papago/n2mt"
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    rescode = response.getcode()
 
-if(rescode==200):
-    response_body = response.read()
-    
-    # Json format
-    result = json.loads(response_body.decode('utf-8'))
-    pprint(result)
+    if(rescode==200):
+        response_body = response.read()
+        
+        # Json format
+        result = json.loads(response_body.decode('utf-8'))
 
-    # Json result  
-    with open('Crawler\\translated_files\\translated_0401_0402.txt', 'w', encoding='utf8') as f:
-        f. write(result['message']['result']['translatedText'])
-else:
-    print("Error Code:" + rescode)
+        # Json result  
+        f. write(result['message']['result']['translatedText'] + "\n")
+    else:
+        print("Error Code:" + rescode)
